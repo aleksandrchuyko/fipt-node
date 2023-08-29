781dee14d1e3b13123cc4c5d8e13b7b4a00becb6
aleksandrchuyko <chao.mess@gmail.com>
2023-08-29 17:51:11 +0300

    Update prepareData.ts

--- src/repositories/prepareData.ts
+++ src/repositories/prepareData.ts
@@ -1,40 +1,19 @@
 import fs from 'fs';
+const { finished } = require('node:stream/promises');
 import { parse } from 'csv-parse';
 import { connectToDatabase } from './connectToDatabase';
 
 export const prepareData = async (filepath = '../public/docs/students.csv') => {
   const db = connectToDatabase();
+  db.serialize(function () {
+    db.run(`DELETE FROM migration`);
+  });
 
-  // await db.run(`DELETE FROM migration`);
+  const rs = fs.createReadStream(filepath, 'utf8');
 
-  await db.get(
-    `SELECT COUNT(*) FROM migration`,
-    function (error: { message: any }, count: any) {
-      if (error) {
-        return console.log(error.message);
-      } else {
-        console.log(count);
-      }
-    }
-  );
-
-  fs.createReadStream(filepath, 'utf8')
-    .pipe(parse({ delimiter: ';', from_line: 2 }))
-    .on('data', function (row) {
-      console.log(row[1], row[5], row[6], row[18], row[19], row[23], row[25]);
-      db.serialize(function () {
-        db.run(
-          `INSERT INTO migration VALUES (?, ?, ? , ?, ?, ?, ?)`,
-          [row[1], row[5], row[6], row[18], row[19], row[23], row[25]],
-          function (error: { message: any }) {
-            if (error) {
-              return console.log(error.message);
-            }
-          }
-        );
-      });
-    });
-    // .end(() => {
+  async function run() {
+    await finished(rs);
+    // db.serialize(function () {
     //   db.get(
     //     `SELECT COUNT(*) FROM migration`,
     //     function (error: { message: any }, count: any) {
@@ -46,4 +25,35 @@ export const prepareData = async (filepath = '../public/docs/students.csv') => {
     //     }
     //   );
     // });
+
+    db.serialize(function () {
+      db.all(
+        `SELECT * FROM migration`,
+        function (error: { message: any }, count: any) {
+          if (error) {
+            return console.log(error.message);
+          } else {
+            console.log(count);
+          }
+        }
+      );
+    });
+  }
+
+  run().catch(console.error);
+
+  rs.pipe(parse({ delimiter: ';', from_line: 2 })).on('data', function (row) {
+    console.log(row[1], row[5], row[6], row[18], row[19], row[23], row[25]);
+    db.serialize(function () {
+      db.run(
+        `INSERT INTO migration VALUES (?, ?, ? , ?, ?, ?, ?)`,
+        [row[1], row[5], row[6], row[18], row[19], row[23], row[25]],
+        function (error: { message: any }) {
+          if (error) {
+            return console.log(error.message);
+          }
+        }
+      );
+    });
+  });
 };
